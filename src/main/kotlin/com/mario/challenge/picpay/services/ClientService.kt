c6ffe6652983b7fae50fb4c6df662bd0cdf388cd
MarioHBS <marioh90@gmail.com>
2023-10-12 19:16:55 -0300

    feat: adicionado ClientService

---
+++ src/main/kotlin/com/mario/challenge/picpay/services/ClientService.kt
@@ -0,0 +1,25 @@
+package com.mario.challenge.picpay.services
+
+import com.mario.challenge.picpay.domain.client.Client
+import com.mario.challenge.picpay.domain.client.UserType
+import com.mario.challenge.picpay.repositories.ClientRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import java.math.BigDecimal
+
+@Service
+class ClientService(@Autowired private val repository: ClientRepository) {
+	fun validateTransaction(sender: Client, amount: BigDecimal) {
+		if (sender.userType == UserType.MERCHANT)
+			throw Exception("Usuário do tipo lojista não está autorizado a realizar transação")
+
+		if (sender.balance < amount)
+			throw Exception("Saldo insuficiente")
+	}
+
+	fun findClientById(id: String): Client {
+		return this.repository.findClientById(id).orElseThrow { Exception("Usuário não encontrado") }
+	}
+
+	fun saveClient(user: Client) = this.repository.save(user)
+}
