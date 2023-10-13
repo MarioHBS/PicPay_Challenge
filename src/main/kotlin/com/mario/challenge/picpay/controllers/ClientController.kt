a9a6419b8590b95762b0fb7bcb1becd737801054
MarioHBS <marioh90@gmail.com>
2023-10-13 00:08:29 -0300

    feat: correção de rota e objeto de requisição do Body

--- src/main/kotlin/com/mario/challenge/picpay/controllers/ClientController.kt
+++ src/main/kotlin/com/mario/challenge/picpay/controllers/ClientController.kt
@@ -8,15 +8,18 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/users")
+@RestController
+@RequestMapping("/users")
 class ClientController() {
     @Autowired
     private lateinit var userService: ClientService
 
     @PostMapping
-    fun createUser(user: ClientDTO): ResponseEntity<Client> {
+    fun createUser(@RequestBody user: ClientDTO): ResponseEntity<Client> {
         val newClient = userService.createClient(user)
         return ResponseEntity(newClient, HttpStatus.CREATED)
     }
